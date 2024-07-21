8e941dec12b121990baa51651cce9c629fd5aa91
Eric <54091924+EricHsia7@users.noreply.github.com>
2024-07-21 16:12:32 +0800

    update

--- src/core/index.ts
+++ src/core/index.ts
@@ -193,6 +193,7 @@ function getColorInRGBA(element: HTMLElement, property: object): RGBA | linearGr
     const conicGradientRegex = /^conic-gradient\((.*)\)$/;
 
     function parseColorStops(parts: string[]): colorStop[] {
+      const positionRegex = /(\d+(cm|mm|in|px|pt|px|em|ex|ch|rem|vw|vh|vmin|vmax|%))$/;
       var colorStops: colorStop[] = [];
       parts.forEach((part) => {
         var matches2 = part.trim().match(positionRegex);
@@ -233,7 +234,6 @@ function getColorInRGBA(element: HTMLElement, property: object): RGBA | linearGr
       }
 
       // Process remaining parts as color stops
-      const positionRegex = /(\d+(cm|mm|in|px|pt|px|em|ex|ch|rem|vw|vh|vmin|vmax|%))$/;
       const colorStops = parseColorStops(parts);
       return { type: 'linear-gradient', direction, colorStops };
     }
